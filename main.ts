0d40939c7af2d54bc664809ac058105f20e13762
Jack Singewald <63979352+DueHack12@users.noreply.github.com>
2024-04-04 06:02:58 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,15 +3,45 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     num = randint(1, 5)
     if (num == 1) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
+        basic.showString("Ask later")
         music.play(music.stringPlayable("F E D C - - - - ", 120), music.PlaybackMode.UntilDone)
     } else if (num == 2) {
+        basic.showString("Hmm.")
+        music.play(music.stringPlayable("D D D D - - - - ", 120), music.PlaybackMode.UntilDone)
+    } else if (num == 3) {
+        basic.showString("Yes!")
+        music.play(music.stringPlayable("F C5 - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    } else if (num == 4) {
+        basic.showString("No!")
+        music.play(music.stringPlayable("F E D C - - - - ", 120), music.PlaybackMode.UntilDone)
+    } else if (num == 5) {
+        basic.showString("Maybe?")
+        music.play(music.stringPlayable("D D D D - - - - ", 120), music.PlaybackMode.UntilDone)
+    }
+})
+// rigged yes
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    if (num == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            # # . # #
+            . . . . .
+            `)
+        basic.showString("Ask later")
+        music.play(music.stringPlayable("F E D C - - - - ", 120), music.PlaybackMode.UntilDone)
+    } else if (num == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # #
+            # # . . .
+            . . . . .
+            . . . . .
+            `)
+        music.play(music.stringPlayable("D D D D - - - - ", 120), music.PlaybackMode.UntilDone)
+    } else if (num == 3) {
         basic.showLeds(`
             . . . . .
             . . . . #
@@ -20,30 +50,23 @@ input.onButtonPressed(Button.A, function () {
             . # . . .
             `)
         music.play(music.stringPlayable("F C5 - - - - - - ", 120), music.PlaybackMode.UntilDone)
-    } else if (num == 3) {
+    } else if (num == 4) {
         basic.showLeds(`
-            . # # # .
             # . . . #
-            . . . . #
-            . . . # .
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        music.play(music.stringPlayable("F E D C - - - - ", 120), music.PlaybackMode.UntilDone)
+    } else if (num == 5) {
+        basic.showLeds(`
+            . # # # #
+            # . # # #
+            . . # . .
+            . . . . .
             . . # . .
             `)
         music.play(music.stringPlayable("D D D D - - - - ", 120), music.PlaybackMode.UntilDone)
-    } else if (num == 4) {
-        basic.showString("Ask later")
-    } else if (num == 5) {
-        basic.showString("Hmm.")
     }
-})
-// rigged yes
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    basic.showLeds(`
-        # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        `)
-    music.play(music.stringPlayable("F E D C - - - - ", 120), music.PlaybackMode.UntilDone)
 })
